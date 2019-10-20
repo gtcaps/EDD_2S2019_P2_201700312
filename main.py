ca5a5c7d750e2b567fb4abdd86405c1db6497573
Aybson Mercado <aybsonmg@gmail.com>
2019-10-19 19:20:56 -0600

    add menu method

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from list import DoublyLinkedList
 from methods import *
+import sys
 
-
+'''
 my_list = DoublyLinkedList()
 my_list.add(data_to_json('file', my_list))
 my_list.add(data_to_json('file2', my_list))
@@ -12,3 +13,32 @@ my_data = data_to_json('file2', my_list)
 my_list.graph_list()
 
 #my_list.print()
+'''
+
+
+def main_menu():
+
+    try:
+
+        option = int(input(""" 
+        -------------- BLOCKCHAIN MENU --------------
+        1. INSERT BLOCK
+        2. SELECT BLOCK
+        3. REPORTS
+        ---------------------------------------------
+        SELECT A OPTION: 
+        """))
+
+        if option is 1:
+            print("SELECCIONASTE 1")
+        elif option is 2:
+            print("SELECCIONASTE 2")
+        elif option is 3:
+            print("SELECCIONASTE 3")
+        else:
+            main_menu()
+    except:
+        main_menu()
+    
+
+main_menu()
